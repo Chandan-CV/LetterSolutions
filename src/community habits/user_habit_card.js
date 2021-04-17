199093d4523109ae7e814aa87b4976e1d77a0bac
Arjun-Mishra-312 <arjunmishra31204@gmail.com>
2021-04-18 01:37:27 +0530

    final community habit

--- src/community habits/user_habit_card.js
+++ src/community habits/user_habit_card.js
@@ -1,15 +1,62 @@
 import { Card, Checkbox } from "@material-ui/core";
-import React from "react";
-import '../community habits/user_habit_card.css'
+import { CheckBox } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import "../community habits/user_habit_card.css";
+import { db } from "../Fire";
+import firebase from "firebase";
+
 const UserHabitCard = () => {
+  const [habits, setHabit] = useState([]);
+  useEffect(async () => {
+    await db
+      .collection("classrooms")
+      .doc("N6UITxdvjwLVUsNFTLNL")
+      .collection("habits")
+      .get()
+      .then((resp) => {
+        setHabit(
+          resp.docs.map((doc) => {
+            return doc.data();
+          })
+        );
+      });
+  }, []);
   return (
-    
-      <Card className="userhabitcard">
-        <p className="title">Yoga</p>
-        <p className="title">Have You Achieved It Today </p>
-        <Checkbox></Checkbox>
-      </Card>
-    
+    <div>
+      {habits.map((item) => (
+        <div>
+          {(() => {
+            if (item.incomplete.indexOf("Ram") > -1) {
+              return (
+                <div className="userhabitcard">
+                  <p className="title">{item.habit_name}</p>
+                  <p className="subtitle">Did You Achieved It Today</p>
+                  <Checkbox
+                    onChange={async () => {
+                      await db
+                        .collection("classrooms")
+                        .doc("N6UITxdvjwLVUsNFTLNL")
+                        .collection("habits")
+                        .doc(item.id)
+                        .update({
+                          completed: firebase.firestore.FieldValue.arrayUnion(
+                            "Ram"
+                          ),
+                          incomplete: firebase.firestore.FieldValue.arrayRemove(
+                            "Ram"
+                          ),
+                        });
+                    }}
+                  ></Checkbox>
+                </div>
+              );
+            } else {
+              return <p></p>;
+            }
+          })()}
+        </div>
+      ))}
+    </div>
   );
 };
 
